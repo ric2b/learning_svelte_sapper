f84c2cd20c255c63f69812cd5a222554439c2113
Ricardo Amendoeira <ricardo.filipe.amendoeira+github@gmail.com>
2020-05-26 22:59:46 +0100

    simplify chart updating

--- src/routes/toy_examples/chart.svelte
+++ src/routes/toy_examples/chart.svelte
@@ -1,19 +1,15 @@
 <script>
-	import { onMount } from 'svelte';
-	// import chartjs from 'chart.js'
+	import { onMount, afterUpdate } from 'svelte';
 	import { Chart } from 'chart.js'
 
 	let canvas;
 	let final_number = 5;
 	let chart;
-
-	let update_last_element = () => {}
-	$: update_last_element(final_number);
-	
+	$: data = [0, 10, 5, null, 20, 30, final_number];
 
 	onMount(async () => {
-		var ctx = canvas.getContext('2d');	
-		var chart = new Chart(ctx, {
+		let ctx = canvas.getContext('2d');	
+		chart = new Chart(ctx, {
 		    // The type of chart we want to create
 		    type: 'line',
 
@@ -24,7 +20,7 @@
 		            label: 'My First dataset',
 		            backgroundColor: 'rgb(255, 99, 132)',
 		            borderColor: 'rgb(255, 99, 132)',
-		            data: [0, 10, 5, null, 20, 30, 45],
+		            data: data,
 		            fill: false
 		        }]
 		    },
@@ -32,15 +28,12 @@
 		    // Configuration options go here
 		    options: {spanGaps: true}
 		});
-		
-		update_last_element = (value) => {
-			chart.data.datasets[0].data[chart.data.datasets[0].data.length - 1] = final_number;
-			chart.update();
-		}
 	})
 
-
-
+	afterUpdate(() => {
+		chart.data.datasets[0].data = data
+		chart.update();
+	});
 </script>
 
 <canvas bind:this={canvas}></canvas>
