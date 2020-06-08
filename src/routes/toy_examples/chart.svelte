47d40d1d9aba60e87cb282d3a9ce10baa259ccd0
Ricardo Amendoeira <ricardo.filipe.amendoeira+github@gmail.com>
2020-06-08 15:52:24 +0100

    add week/month/year/all buttons

--- src/routes/toy_examples/chart.svelte
+++ src/routes/toy_examples/chart.svelte
@@ -2,9 +2,7 @@
     import { onMount, afterUpdate } from 'svelte';
     import { Chart } from 'chart.js'
     import 'chartjs-adapter-date-fns';
-    import formatISO from 'date-fns/formatISO'
-    import min from 'date-fns/min'
-    import max from 'date-fns/max'
+    import { formatISO, min, max, startOfDay, sub } from 'date-fns'
 
     let canvas;
     let start_date = formatISO(new Date(), { representation: 'date' });
@@ -13,12 +11,12 @@
 
     function generate_data() {
         return {
-            t: new Date(Date.parse('2012') + Math.random() * (Date.parse('2020') - Date.parse('2012'))), 
+            t: new Date(Date.parse('2018') + Math.random() * (Date.parse('2020') - Date.parse('2018'))), 
             y: Math.random() * 100 
         }
     }
 
-    let raw_data = [...Array(50)].map(_x => generate_data()).sort((a, b) => a.t - b.t);
+    let raw_data = [...Array(200)].map(_x => generate_data()).sort((a, b) => a.t - b.t);
     /*let raw_data = [
         {t: new Date('2013'), y: 0}, 
         {t: new Date('2014'), y: 10}, 
@@ -31,20 +29,20 @@
 
     $: data = raw_data.filter(({t, y}) => Date.parse(start_date) <= t && t <= Date.parse(end_date))
     //$: min_date = new Date(Math.min(...raw_data.map(x => x.t))).toDateString()
-    $: min_date = formatISO(min(raw_data.map(x => x.t)), { representation: 'date' })
-    $: max_date = formatISO(max(raw_data.map(x => x.t)), { representation: 'date' })
+    $: min_date = min(raw_data.map(x => x.t))
+    $: max_date = max(raw_data.map(x => x.t))
 
-    $: start_date = min_date
-    $: end_date = max_date
+    $: start_date = isoDate(min_date)
+    $: end_date = isoDate(max_date)
 
     let from;
     let to;
 
-    $: if(from) { from.min = min_date; from.max = max_date; }
-    $: if(to) { to.min = min_date; to.max = max_date; }
+    $: if(from) { from.min = isoDate(min_date); from.max = isoDate(max_date); }
+    $: if(to) { to.min = isoDate(min_date); to.max = isoDate(max_date); }
 
     //$: console.log(Date.parse('2018-03-12'))
-    //$: console.log(min_date)
+    $: console.log(startOfDay(sub(new Date(), {weeks: 1})))
     //$: console.log([...Array(5)].map(_x => generate_data()))
 
     onMount(async () => {
@@ -82,9 +80,17 @@
         chart.data.datasets[0].data = data
         chart.update();
     });
+
+    const isoDate = date => formatISO(date, { representation: 'date' })
+    const dateSub = (date, options) => isoDate(startOfDay(sub(date, options)))
 </script>
 
 <canvas bind:this={canvas}></canvas>
 
 <label>Start date<input bind:this={from} type=date bind:value={start_date}></label>
 <label>End date<input bind:this={to} type=date bind:value={end_date}></label>
+
+<button on:click={()=> { start_date = dateSub(max_date, { weeks: 1 }) }}>1W</button>
+<button on:click={()=> { start_date = dateSub(max_date, { months: 1 }) }}>1M</button>
+<button on:click={()=> { start_date = dateSub(max_date, { years: 1 }) }}>1Y</button>
+<button on:click={()=> { start_date = isoDate(min_date) }}>All</button>
